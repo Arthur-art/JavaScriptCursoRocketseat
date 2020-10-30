53958cfacfd1548c2aa5170a03206a4ffef1cff0
Arthur-art <arthurteixeira.guts@gmail.com>
2020-10-30 20:32:04 -0300

    Exercicio02 promises

--- JavaScriptAssincrono/ExerciciosJSModulo4/Exercicio02/app.js
+++ JavaScriptAssincrono/ExerciciosJSModulo4/Exercicio02/app.js
@@ -3,12 +3,13 @@ nome do usuário e clicar no botão buscar a aplicação deve buscar pela API do
 URL abaixo) os dados de repositórios do usuário e mostrá-los em tela:
 URL de exemplo: https://api.github.com/users/arthur-art/repos*/
 
-var input = document.querySelector('#input input');
 var button = document.querySelector('#input button');
-var ul = document.querySelector('#ul');
-var getUser = function(){
+var input = document.querySelector('#input input');
+var ulElement = document.querySelector('#ul');
+
+var myPromise = function(){
     return new Promise(function(resolve, reject){
-       
+
         var xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://api.github.com/users/'+input.value);
         xhr.send(null);
@@ -16,29 +17,35 @@ var getUser = function(){
         xhr.onreadystatechange=function(){
             if(xhr.readyState===4){
                 if(xhr.status===200){
-                resolve(JSON.parse(xhr.responseText));
-            }else{
-                reject('Error');
+                    resolve(JSON.parse(xhr.responseText));
+                }else{
+                    reject('error');
+                }
             }
-         }
         }
     });
 }
-button.onclick = function(){
-getUser()
-.then(function(response){
-    console.log(response.blog);
-    var textblog = document.createTextNode(JSON.stringify(response.blog));
-    console.log(textblog);
-    var li = document.createElement('li');
 
-    ul.appendChild(li);
-    li.appendChild(textblog);
-    
-})
-.catch(function(error){
-    console.warn(error);
-});
-};
+button.onclick = function(){
+    myPromise()
+    .then(function(response){
+        console.log(response);
+
+        var aElement = document.createElement('a');
+        aElement.setAttribute('href', response.avatar_url);
+        var aText = document.createTextNode('FotoPerfil');
+        aElement.appendChild(aText);
+        var liElement = document.createElement('li');
+        liElement.appendChild(aElement);
+        ulElement.appendChild(liElement);
+
+    })
+    .catch(function(error){
+        alert(error);
+    });
+}
+
+
+
 
 
