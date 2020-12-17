1a418bae68efb1ffcad914c61b013c098353df0d
Arthur-art <arthurteixeira.guts@gmail.com>
2020-12-17 09:10:10 -0300

    Manipulando array

---
+++ ManipulandoArrays/app.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3];
+//adicionar no fim
+arr.push(4);
+//adicionar no começo
+arr.unshift(12);
+//remover do fim
+arr.pop();
+//remover do começo
+arr.shift();
+//pegar somente alguns elementos do array
+console.log(arr.slice(1, 2));
+
+//remover 1 ou mais itens em qualquer posição do array
+
+
+console.log(arr);
