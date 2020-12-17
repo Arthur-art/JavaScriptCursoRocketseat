23f87df8ba574fb69e76cf0fe35d10213442bd87
Arthur-art <arthurteixeira.guts@gmail.com>
2020-12-17 09:28:13 -0300

    Manipulando array

--- ManipulandoArrays/app.js
+++ ManipulandoArrays/app.js
@@ -1,4 +1,4 @@
-let arr = [1,2,3];
+let arr = ["html","css","javascript","nodejs","react","reactnative"];
 //adicionar no fim
 arr.push(4);
 //adicionar no começo
@@ -8,9 +8,12 @@ arr.pop();
 //remover do começo
 arr.shift();
 //pegar somente alguns elementos do array
-console.log(arr.slice(1, 2));
-
+//console.log(arr.slice(1, 2));
 //remover 1 ou mais itens em qualquer posição do array
+//arr.splice(1, 3)
+//encontrar a posicao de um elemento no array
+let index = arr.indexOf("nodejs");
+arr.splice(index, 1);
 
 
 console.log(arr);
