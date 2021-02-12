9398484a0df5f3c9939c83e5558131817b0f503e
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-12 10:21:31 -0300

    Function mostrando valores repetidos em um array

--- ExcluindoValoresRepetidosArray/app.js
+++ ExcluindoValoresRepetidosArray/app.js
@@ -1,43 +1,42 @@
-function limpandoRepetidos(array){
-    for(let i in array){
-        let valorcomp = array[i];
-        let cont = 0;
-        for(let i in array){
-            if(valorcomp===array[i]){
-                cont++;
-                if(cont>1){
-                    cont--;
-                    delete array[i];
-                }
-            }
-        }
-    }
-    return array;
-}
+let array2 = ["arthur", "ivar", "bk201", "arthur"];
 
-
-
-let array2 = ["arthur","ivar","bk201","arthur"];
-
-function mostrandoRepeditos(array){
-
-let valorigual = [];
-
-for(let i in array){
-    let valorcomp = array[i];
-    cont=0;
-    for(let i in array){
-        if(valorcomp===array[i]){
-            cont++;
-            if(cont>1){
+const Utils = {
+    mostrandoRepeditos(array) {
+        let valorigual = [];
+      
+        for (let i in array) {
+          let valorcomp = array[i];
+          cont = 0;
+          for (let i in array) {
+            if (valorcomp === array[i]) {
+              cont++;
+              if (cont > 1) {
                 cont--;
                 valorigual.push(array[i]);
                 delete array[i];
+              }
             }
+          }
         }
-    }
-}
-return valorigual;
+        return valorigual;
+      },
+
+      limpandoRepetidos(array) {
+        for (let i in array) {
+          let valorcomp = array[i];
+          let cont = 0;
+          for (let i in array) {
+            if (valorcomp === array[i]) {
+              cont++;
+              if (cont > 1) {
+                cont--;
+                delete array[i];
+              }
+            }
+          }
+        }
+        return array;
+      }
 }
 
-console.log(mostrandoRepeditos(array2));
+console.log(Utils.mostrandoRepeditos(array2));
