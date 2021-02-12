61d9a8372dbf1a729a9dedaacaac0790dd12e007
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-12 14:22:55 -0300

    Exercicio -1 Js

--- ExcluindoMostrandoValoresRepetidosArray/app.js
+++ ExcluindoMostrandoValoresRepetidosArray/app.js
@@ -1,42 +1,39 @@
 let array2 = ["arthur", "ivar", "bk201", "arthur", "ivar"];
 
-const Utils = {
-    mostrandoRepeditos(array) {
-        let valorigual = [];
-      
-        for (let i in array) {
-          let valorcomp = array[i];
-          cont = 0;
-          for (let i in array) {
-            if (valorcomp === array[i]) {
-              cont++;
-              if (cont > 1) {
-                cont--;
-                valorigual.push(array[i]);
-                delete array[i];
-              }
-            }
+const Utils={
+  mostrandoRepetidos(array){
+    valorRepetido=[];
+    for(let i in array){
+      let valorcomp=array[i];
+      cont=0;
+      for(let i in array){
+        if(valorcomp===array[i]){
+          cont++;
+          if(cont>1){
+            cont--;
+            valorRepetido.push(array[i]);
+            delete(array[i]);
           }
         }
-        return valorigual;
-      },
-
-      limpandoRepetidos(array) {
-        for (let i in array) {
-          let valorcomp = array[i];
-          let cont = 0;
-          for (let i in array) {
-            if (valorcomp === array[i]) {
-              cont++;
-              if (cont > 1) {
-                cont--;
-                delete array[i];
-              }
-            }
-          }
-        }
-        return array;
       }
+    }
+    return valorRepetido;
+  },
+  deletandoRepetidos(array){
+    for(let i in array){
+      let valorcomp=array[i];
+      let cont=0;
+      for(let i in array){
+        if(valorcomp===array[i]){
+          cont++;
+          if(cont>1){
+            cont--;
+            delete array[i];
+          }
+        }
+      }
+    }
+    return array;
+  }
 }
-
-console.log(Utils.mostrandoRepeditos(array2));
+console.log(Utils.mostrandoRepetidos(array2));
