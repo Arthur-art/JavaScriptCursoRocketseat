cdea33e1e5c96afeab9b27883622713be851cd13
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-13 11:46:58 -0300

    ManipulandoDados Js

--- ManipulandoDados/app.js
+++ ManipulandoDados/app.js
@@ -1,2 +1,8 @@
-
-
+function Person(nome,idade,cidade,rua){
+    this.nome=nome;
+    this.idade=idade;
+    this.cidade=cidade;
+    this.rua=rua;
+}
+const arthur=new Person("Arthur",24,"Contagem","Milanez");
+console.log(arthur);
