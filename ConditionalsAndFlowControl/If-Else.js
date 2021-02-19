221db48dc4a736b5e378952bc59d5a3afe37cfed
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-19 17:26:58 -0300

    Conditionals and flow control

---
+++ ConditionalsAndFlowControl/If-Else.js
@@ -0,0 +1,11 @@
+let temperature = 40;
+
+if(temperature == 37){
+    console.log('baixa febre')
+}else if(temperature == 38){
+    console.log('febre media/alta')
+}else if(temperature = 39 || temperature >=40){
+    console.log('febre alta')
+}else{
+    console.log('temperatura normal')
+}
--- ExpressionsAndOperators/ExpressionsAndOperators.js
+++ ExpressionsAndOperators/ExpressionsAndOperators.js
@@ -2,12 +2,41 @@
 
 //Binary
 let number = 1;
-console.log(number+1);
+//console.log(number+1);
 
 //Unary
 
-console.log(++number);
+//console.log(++number);
 
 //Ternary
 
-console.log(true ? "ok" : "not ok");
+//console.log(true ? "ok" : "not ok");
+
+/*FALSY
+    Quando um valor é considerado false em contextos onde um booleano é obrigatorio(condicionais e loops)
+
+    false
+    0
+    -0
+    ""
+    null
+    undefined
+    NaN
+*/
+console.log(0 ? 'verdadeiro' : 'falso');
+
+/*TRUTHY
+    Quando um valor é considerado true em contextos onde um booleano é obrigatorio(condicionais e loops)
+
+    true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infiniy
+    -Infinity
+*/
+console.log("0" ? 'verdadeiro' : 'falso');
