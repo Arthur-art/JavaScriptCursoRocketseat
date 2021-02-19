b6ae44db17a31a30000c53347f283e599637e06f
Arthur-art <arthurteixeira.guts@gmail.com>
2021-02-18 22:25:58 -0300

    Expressions and operators

---
+++ ExpressionsAndOperators/OperatorsLogical.js
@@ -0,0 +1,16 @@
+let pao = true;
+let queijo = true;
+
+//AND &&
+console.log(pao&&queijo);
+
+//OR ||
+console.log(pao||queijo);
+
+//NOT !
+console.log(!pao)
+
+//Ternario
+const result = pao||queijo ? 'ok' : 'not ok';
+
+console.log(result);
